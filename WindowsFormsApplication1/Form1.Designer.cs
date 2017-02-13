51e0bbb50219b1b7b1ef05bf5de0ebc95e0e0cbd
nope! <nope!@DESKTOP-L200R81>
2017-02-13 09:38:07 +0530

    v0.3

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -101,11 +106,65 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Upper bound...";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(2, 417);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(609, 22);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 36);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Instructions";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(475, 24);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 21);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Only even";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(475, 72);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 21);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Only odd";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(475, 118);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(58, 21);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "beta";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 244);
+            this.ClientSize = new System.Drawing.Size(616, 441);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
@@ -130,6 +189,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -25,16 +25,56 @@ namespace WindowsFormsApplication1
             int n = int.Parse(textBox1.Text);
             int l = int.Parse(textBox2.Text);
             int u = int.Parse(textBox3.Text);
-            for (int i = 0; i < n; i++)
+            int a = 1,b=0;
+            if (checkBox1.Checked)
             {
-                long g = Rand.Next(l, u);
-                nlist.Add(g.ToString());
+                a = 2;
+                n = n / 2;
             }
-            Clipboard.Clear();
-            string s1 = "";
-            foreach (object item in nlist) s1 += item.ToString() + "\r\n";
-            Clipboard.SetText(s1);
-            MessageBox.Show("Copied to clipboard.");
+
+            else if (checkBox2.Checked)
+            {
+                b = 1;
+                n = n / 2;
+            }
+            if(checkBox1.Checked && checkBox2.Checked)
+            {
+                a = 1;
+                b = 0;
+                n = 2 * n;
+            }
+                
+            if (n <= 10000)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    long g = a*Rand.Next(l, u)+b;
+                    progressBar1.Value = 100*i/n;
+                    nlist.Add(g.ToString());
+                }
+                Clipboard.Clear();
+                string s1 = "";
+                foreach (object item in nlist) s1 += item.ToString() + "\r\n";
+                Clipboard.SetText(s1);
+                MessageBox.Show("Copied to clipboard.");
+            }
+            else
+            {
+                string path = @"d:\TestFile.txt";
+                //File.CreateText(path);
+                int j = n;
+                while(n>0)
+                {
+                    long g = a*Rand.Next(l, u)+b;
+                    progressBar1.Value = 100*(j - n - 1)/j;
+                    string appendText = g.ToString() + Environment.NewLine;
+                    File.AppendAllText(path, appendText);
+                    n--;
+                }
+
+                MessageBox.Show("File created in "+path);
+            }
+            progressBar1.Value = 0;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -43,6 +83,21 @@ namespace WindowsFormsApplication1
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This portable software can be used to generate test cases. For n<=50000 it can take upto 2 seconds for generating cases and coping to your clipboard. For n>100000 the taken time is very high, I do not recomend you to generate random data for n>100000. But in case you want to generate random test data at any cost you are recomended to stay calm. For n>=100000 a TXT file is created in your D drive.                                                   -mky");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
